badec0b43390da25bc51e77267a78d122ece1df9
rob4abcba <rob_abcba@yahoo.com>
2023-10-30 12:15:10 -0700

    Add Header.js component. Wrap body with Container tags and main tags.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+// import { Container } from "react-bootstrap/lib/Tab";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 
 const App = () => {
-  return <h1>Welcome to LiveAuction</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to LiveAuction</h1>;
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default App;
