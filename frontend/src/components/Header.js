ba127ed2dfd0d648562b598655df8eed129b0deb
rob4abcba <rob_abcba@yahoo.com>
2023-10-30 12:33:32 -0700

    Change expand=lg to sm so Cart and SignIn icons don't collapse into a HamburgerMenu until we shrink to small width

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,7 @@ import { FaShoppingCart, FaUser } from "react-icons/fa";
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="sm" collapseOnSelect>
         <Container>
           <Navbar.Brand href="/">LiveAuction</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
