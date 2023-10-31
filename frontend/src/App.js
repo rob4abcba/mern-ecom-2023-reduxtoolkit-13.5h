b16da984322ff401b876eafc89db35e5dd8c4987
rob4abcba <rob_abcba@yahoo.com>
2023-10-31 08:03:01 -0700

    Remove extraneous semicolon.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to LiveAuction</h1>;
+          <h1>Welcome to Live Auction</h1>
         </Container>
       </main>
       <Footer />
