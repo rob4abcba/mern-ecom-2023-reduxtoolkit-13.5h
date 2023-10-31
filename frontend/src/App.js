390d5475604999000e922e667951b81669f67c67
rob4abcba <rob_abcba@yahoo.com>
2023-10-31 10:57:24 -0700

    Replace direct HomeScreen with indirect HomeScreen via Outlet from react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 // import { Container } from "react-bootstrap/lib/Tab";
 // import { Container } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
-
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
+// import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
@@ -13,7 +13,8 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          {/* <HomeScreen /> */}
+          <Outlet />
         </Container>
       </main>
       <Footer />
